aab2ae1d34b6592d7169614c03d50788fbbfdcf0
RachitSinghh <singhrachit@outlook.in>
2025-06-17 19:58:41 +0530

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountID = 13354; 
+let accountEmail = "Rachit@mail.com"
+var accountCity = "ghazibad";
+
+console.table([accountID, accountEmail,accountCity]);
+
+/*
+prefer not to use var, 
+because of issue in block scope and functional scope
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict" // treat all js code as newer version 
+
+console.log(3 + 3);
+
+
+// code readabilty should be high 
+// number => 2 to power 53
+// bigint 
+// String => ""
+// boolean => True, False
+//  null
+// undefined => 
+// symbol
+
+console.log(typeof undefined)
+console.log(typeof null);
