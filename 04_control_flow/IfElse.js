0d84b5daa66d9090d053ef0151cbdd15a9f4f460
RachitSinghh <singhrachit@outlook.in>
2025-06-22 22:58:13 +0530

    control flow learning

---
+++ 04_control_flow/IfElse.js
@@ -0,0 +1,50 @@
+// if
+
+
+// const isUserlogin = false
+// const temperature = 41; 
+// if(temperature === 41){
+//  console.log('less than 50');
+// }else{
+//   console.log("Temperature is greater than 50");
+  
+// }
+
+// const score =  200; 
+
+// if(score > 100){
+//   const power = "fly"
+//   console.log(`User power: ${power}`);
+// }
+
+// short hand notation
+
+const balance = 1000; 
+
+// if(balance > 500) console.log("test"); not to do 
+
+// if(balance < 500){
+//   console.log("less than 500");
+// }else if(balance < 750){
+//   console.log("less than 750");
+// }else if(balance < 900){
+//   console.log("less than 900");
+// }else{
+//   console.log("Less than 1200");
+// }
+
+const userLoggedIn = true;
+const debitCard = true;
+
+const loggedInFromGoogle = true;
+const loggedInFromEmail = false;
+
+if(userLoggedIn && debitCard){
+  console.log("Allow to buy courses");
+};
+
+if(loggedInFromGoogle || loggedInFromGoogle ){
+  console.log("User logged in");
+}
+
+
